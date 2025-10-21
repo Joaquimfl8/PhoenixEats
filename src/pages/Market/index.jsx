fcebcfb8e566d7b1e2c1ae36e7d2d941e47d2373
Joaquimfl8 <4553543698@estudante.sed.gov.br>
2025-10-21 16:45:18 -0300

    market

--- src/pages/Market/index.jsx
+++ src/pages/Market/index.jsx
@@ -1,1 +1,17 @@
 import styles from "./Market.module.css";
+import Header from "../../components/Header";
+
+function Market() {
+    return (
+        <>
+
+        <Header />
+        <h2>Carrinho</h2>
+        <div className={styles.market_components}>
+            
+        </div>
+        </>
+    )
+}
+
+export default Market
