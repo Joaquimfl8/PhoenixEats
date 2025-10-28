67eb03e71ce9360a7df095e401ea332d3c73a7b8
MigelEspig <4545193527@estudante.sed.sc.gov.br>
2025-10-28 15:40:40 -0300

    TableFezes

--- src/pages/Market/index.jsx
+++ src/pages/Market/index.jsx
@@ -10,24 +10,26 @@ function Market() {
         <Header />
         <h2>Carrinho</h2>
         <div className={styles.market_components}>
-            <table className={styles.market_components}>
-                <thead className={styles.market_head}>
-                    <tr className={styles.head_components}>
-                        <th>Produto</th>
-                        <th>Preço</th>
-                        <th>Descrição</th>
-                    </tr>
-                </thead>
-                <tbody className={styles.market_body}>
-                    {Object.values(ProductsData).map(product => (
-                        <tr className={styles.body_components}>
-                            <td><img src={product.img} alt={product.title} />{product.title}</td>
-                            <td>{product.price}</td>
-                            <td>{product.desc}</td>
+            <div className={styles.table_container}> 
+                <table className={styles.market_components}>
+                    <thead className={styles.market_head}>
+                        <tr className={styles.head_components}>
+                            <th>Produto</th>
+                            <th>Preço</th>
+                            <th>Descrição</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className={styles.market_body}>
+                        {Object.values(ProductsData).map(product => (
+                            <tr className={styles.body_components}>
+                                <td><img src={product.img} alt={product.title} />{product.title}</td>
+                                <td>{product.price}</td>
+                                <td>{product.desc}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>  
             <Footer/>
         </div>
         </>
